9abcd2ab3b28b218ed739e984244ba8bd902ea59
Rodrigo Barrera <rbarrera028@gmail.com>
2023-12-19 02:48:59 -0300

    commit

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# React + Vite
+# Ejercicio Unicafe Freecodecamp
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Como la mayoría de las empresas, Unicafe recopila comentarios de sus clientes. Tu tarea es implementar una aplicación web para recopilar comentarios de los clientes. Solo hay tres opciones para los comentarios: good (bueno), neutral y bad(malo).
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+La aplicación debe mostrar el número total de comentarios recopilados para cada categoría.
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,68 @@
 import { useState } from 'react';
 
+const Button = ({ text, handleClick }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>
+      {text} {value}
+    </p>
+  );
+};
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  } else
+    return (
+      <>
+        <h2>statistics</h2>
+        <StatisticLine text={'good'} value={good}></StatisticLine>
+        <StatisticLine text={'neutral'} value={neutral}></StatisticLine>
+        <StatisticLine text={'bad'} value={bad}></StatisticLine>
+        <StatisticLine text={'total'} value={total}></StatisticLine>
+        <StatisticLine text={'average'} value={average}></StatisticLine>
+        <StatisticLine text={'positive'} value={`${positive}%`}></StatisticLine>
+      </>
+    );
+};
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = good + bad + neutral;
+  const average = (good - bad) / total;
+  const positive = (100 * good) / total;
 
-  return <div>code here</div>;
+  const handleClickGood = () => {
+    setGood(good + 1);
+  };
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+  const handleClickBad = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleClickGood} text={'good'}></Button>
+      <Button handleClick={handleClickNeutral} text={'neutral'}></Button>
+      <Button handleClick={handleClickBad} text={'bad'}></Button>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positive={positive}
+      ></Statistics>
+    </div>
+  );
 };
 
 export default App;
